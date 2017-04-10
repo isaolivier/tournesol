9abad73ec9d5efcb7f3046857f5baed358f928e2
Isabelle OLIVIER <isamaigret@gmail.com>
2017-04-10 17:07:36 +0200

    Spring boot initializr

---
+++ tournesol-service/src/main/java/com/tournesol/TournesolServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tournesol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournesolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournesolServiceApplication.class, args);
+	}
+}
