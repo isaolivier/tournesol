c085df35614a1670723132f08ff82931f245da04
Isabelle OLIVIER <iolivier@pagesjaunes.fr>
2017-06-30 15:45:42 +0200

    Ajout des pages + tabs

---
+++ tournesol-front/src/components/Tournee.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div>
+    Slider des dates
+    Agenda
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'tournee',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
