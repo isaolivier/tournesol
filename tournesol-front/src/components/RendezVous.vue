397a8214af1c532e0e6dc7fcebe1a6565b561670
isaolivier <isamaigret@gmail.com>
2017-07-02 22:18:36 +0200

    Paramétrisation du rendez-vous

--- tournesol-front/src/components/RendezVous.vue
+++ tournesol-front/src/components/RendezVous.vue
@@ -1,16 +1,16 @@
 <template>
-  <svg width="550" height="150">
+  <svg width="800" height="800">
 
-    <rect x="90" y="15" rx="15" ry="15" width="450" height="128"
+    <rect x="90" :y="yDebut" rx="15" ry="15" width="450" :height="height"
           style="fill:white;stroke:lightblue;stroke-width:2;opacity:0.9"/>
-    <text x="100" y="40">Alexandre Durand</text>
+    <text x="100" :y="yDebut + 25">{{nom}}</text>
 
     <a href="http://isadanslesnuages.fr">
-      <icon name="phone" scale="1.8" x="250px" y="20px"/>
+      <icon name="phone" scale="1.7" x="250px" :y="yDebut + 5"/>
     </a>
 
     <a href="http://isadanslesnuages.fr">
-      <icon name="waze" scale="1.8" x="290px" y="20px"/>
+      <icon name="waze" scale="1.7" x="290px" :y="yDebut + 5"/>
     </a>
   </svg>
 </template>
@@ -18,9 +18,18 @@
 <script>
   export default {
     name: 'rdv',
-
+    props: ['hourIntervall', 'nom', 'debut', 'fin'],
     data () {
-      return
+      return {
+      }
+    },
+    computed: {
+      yDebut: function () {
+        return this.debut * this.hourIntervall + 15
+      },
+      height: function () {
+        return this.hourIntervall * (this.fin - this.debut) - 10
+      }
     }
   }
 </script>
--- tournesol-front/src/components/Tournee.vue
+++ tournesol-front/src/components/Tournee.vue
@@ -2,17 +2,25 @@
 
   <div>
     Slider des dates<br/>
+
     <svg :width="fullWidth" :height="fullHeight">
 
       <!-- Heures -->
       <text v-for="(hour, index) in hours" x="0" :y="index * hourIntervall + 15">{{hour}}:00</text>
-      <line v-for="(hour, index) in hours" x1="50" :y1="index * hourIntervall + 10" :x2="lineWidth"
-            :y2="index * hourIntervall + 10" style="stroke:rgb(0,0,0);stroke-width:1"/>
-      <line v-for="(hour, index) in hours" x1="50" :y1="index * hourIntervall + 33" :x2="lineWidth"
-            :y2="index * hourIntervall + 33" style="stroke:rgb(180,180,180);stroke-width:0.5"/>
+
+      <line v-for="(hour, index) in hours" x1="50" :x2="lineWidth"
+            :y1="index * hourIntervall + 10"
+            :y2="index * hourIntervall + 10"
+            style="stroke:rgb(0,0,0);stroke-width:1"/>
+
+      <line v-for="(hour, index) in hours" x1="50" :x2="lineWidth"
+            :y1="index * hourIntervall + 10 + (hourIntervall/2)"
+            :y2="index * hourIntervall + 10 + (hourIntervall/2)"
+            style="stroke:rgb(180,180,180);stroke-width:0.5"/>
 
       <!-- Rendez-vous -->
-      <rdv></rdv>
+      <rdv :hourIntervall="hourIntervall" nom="Alexandre Durand" debut="1" fin="3"></rdv>
+      <rdv :hourIntervall="hourIntervall" nom="David Raluy" debut="6" fin="7"></rdv>
     </svg>
   </div>
 
@@ -27,17 +35,30 @@
     data () {
       return {
         hours: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
-        events: [
-          {begin: 8, end: 10},
-          {begin: 14, end: 15}
-        ],
-        fullWidth: document.documentElement.clientWidth,
-        fullHeight: document.documentElement.clientHeight - 200,
-        lineWidth: document.documentElement.clientWidth - 200,
-        hourIntervall: 46
+        fullWidth: window.innerWidth,
+        fullHeight: 1000
       }
     },
-
+    computed: {
+      lineWidth: function () {
+        return this.fullWidth - 100
+      },
+      hourIntervall: function () {
+        return (this.fullHeight - 150) / 11
+      }
+    },
+    methods: {
+      handleWindowResize (event) {
+        this.fullWidth = event.currentTarget.innerWidth
+        this.fullHeight = event.currentTarget.innerHeight
+      }
+    },
+    beforeDestroy: function () {
+      window.removeEventListener('resize', this.handleWindowResize)
+    },
+    mounted () {
+      window.addEventListener('resize', this.handleWindowResize)
+    },
     components: {
       'rdv': RendezVous
     }
