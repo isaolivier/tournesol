105959e00715a4416f2884b562cf01f95d66c8a6
David RALUY <draluy.externe@pagesjaunes.fr>
2017-09-15 10:47:36 +0200

    Tell elemnt js to speak french.

--- tournesol-front/src/main.js
+++ tournesol-front/src/main.js
@@ -6,9 +6,11 @@ import ElementUI from 'element-ui'
 import router from './router'
 import VueResource from 'vue-resource'
 import 'font-awesome/css/font-awesome.css'
+import locale from 'element-ui/lib/locale/lang/fr'
+
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale })
 Vue.use(VueResource)
 
 import 'element-ui/lib/theme-default/index.css'
