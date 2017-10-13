5bbdfa0a4487fa7682cb449216401cb5cf2a317b
Isabelle OLIVIER <iolivier@pagesjaunes.fr>
2017-10-13 17:07:14 +0200

    Init de la base sur fichier local, intégration de flyway

--- tournesol-back/src/main/resources/db/migration/V1.0__init.sql
+++ tournesol-back/src/main/resources/db/migration/V1.0__init.sql
@@ -1,27 +1,30 @@
+create sequence adresse_sequence;
 create table adresse (
-  id bigint not null,
+  id bigint not null default adresse_sequence.nextval primary key,
   code_postal varchar(255),
   commune varchar(255),
   latitude double,
   longitude double,
   numero varchar(255),
   place_id varchar(255),
-  voie varchar(255),
-  primary key (id));
+  voie varchar(255));
 
+
+create sequence appareil_sequence;
 create table appareil (
-  id bigint not null,
+  id bigint not null default appareil_sequence.nextval primary key,
   denomination varchar(255),
   marque varchar(255),
   numero_serie varchar(255),
   client_id varchar(255),
   date_installation date,
   date_mise_en_service date,
-  adresse_id bigint,
-  primary key (id));
+  adresse_id bigint);
 
+
+create sequence client_sequence;
 create table client (
-  id bigint not null,
+  id bigint not null default client_sequence.nextval primary key,
   societe varchar(255),
   civilite varchar(255),
   nom varchar(255),
@@ -29,11 +32,12 @@ create table client (
   portable varchar(255),
   telephone varchar(255),
   note integer,
-  adresse_id bigint,
-  primary key (id));
+  adresse_id bigint);
 
+
+create sequence entreprise_sequence;
 create table entreprise (
-  id bigint not null,
+  id bigint not null default entreprise_sequence.nextval primary key,
   nom varchar(255),
   siret varchar(255),
   heure_ouverture time,
@@ -42,12 +46,13 @@ create table entreprise (
   search_days integer,
   search_distance integer,
   temps_rdv integer,
-  time_step integer,
-  primary key (id));
+  time_step integer);
 
+
+create sequence rendezvous_sequence;
 create table rendezvous (
-  id bigint not null,
+  id bigint not null default rendezvous_sequence.nextval primary key,
   event_cal_id varchar(255),
   appareil_id bigint,
-  client_id bigint,
-  primary key (id));
+  client_id bigint);
+
