d79d2b529be51a3a060143ef316f7ab355d91eca
Isabelle OLIVIER <iolivier@pagesjaunes.fr>
2018-02-22 11:47:03 +0100

    Passage en paramètre du host pour installation en prod

--- tournesol-front/config/prod.env.js
+++ tournesol-front/config/prod.env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  URL_BACK: '"http://tournesol.samsonmultiservices.fr"'
 }
--- tournesol-front/config/test.env.js
+++ tournesol-front/config/test.env.js
@@ -2,5 +2,6 @@ var merge = require('webpack-merge')
 var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  URL_BACK: '"localhost:8080"'
 })
